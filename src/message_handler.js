71f7c39e978dc120d996069aa81fece631f3c255
Matthew Healy <matthew.healy@soundcloud.com>
2019-12-23 17:13:00 +0100

    Unit tests MessageHandler

--- src/message_handler.js
+++ src/message_handler.js
@@ -9,34 +9,36 @@ class MessageHandler {
     const messageTokens = message.content.toLowerCase().split(/ +/);
     const orgName = 'tory-toolkit';
 
-    if (!messageTokens[0] === 'orgbot') return;
+    if (messageTokens[0] !== 'orgbot') return;
 
     const command = messageTokens[1];
     const user = messageTokens[2];
 
-    if (command === 'check') {
-      if (messageTokens.length === 3) {
-        this.github.checkMembership(orgName, user, resp => {
-          message.reply(resp);
-        });
-      } else {
-        message.reply(`Invalid command. Try
-        \`orgbot check USERNAME \` -- check if GitHub user USERNAME is a member of the organisation`);
+    const isRecognisedCommand = ['check', 'invite'].includes(command);
+    if (isRecognisedCommand) {
+      if (command === 'check') {
+        if (messageTokens.length === 3) {
+          this.github.checkMembership(orgName, user, resp => {
+            message.reply(resp);
+          });
+        } else {
+          message.reply('Invalid use of `orgbot check`. Try `orgbot check <YOUR_GITHUB_USERNAME>` instead.');
+        }
       }
-    }
 
-    if (command === 'invite') {
-      if (messageTokens.length === 3) {
-        this.github.inviteMember(orgName, user, resp => {
-          message.reply(resp);
-        });
-      } else {
-        message.reply(`Invalid command. Try
-        \`orgbot invite USERNAME\` -- invite GitHub user USERNAME to the organisation`);
+      if (command === 'invite') {
+        if (messageTokens.length === 3) {
+          this.github.inviteMember(orgName, user, resp => {
+            message.reply(resp);
+          });
+        } else {
+          message.reply('Invalid use of `orgbot invite`. Try `orgbot invite <YOUR_GITHUB_USERNAME>` instead.');
+        }
       }
+    } else {
+      message.reply(`I don't understand the command \`${command}\`.`)
     }
   }
-
 }
 
 module.exports = MessageHandler;
